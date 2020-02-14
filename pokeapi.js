818a025c6bf961fb353c4c9bd3028366778d3821
zwyak <vlad.senyuk97@gmail.com>
2020-02-14 16:35:39 +0300

    Updated pokeapi.js (errors)

--- pokeapi.js
+++ pokeapi.js
@@ -1,6 +1,6 @@
 const Promise = require('bluebird');
 const axios = require('axios');
-
+//get pokemon by id=42
 axios.get('https://pokeapi.co/api/v2/pokemon/42')
   .then(function (response) {
     console.log(`Name: ${response.data.name} Height: ${response.data.height} Weight: ${response.data.weight}`);
@@ -10,3 +10,49 @@ axios.get('https://pokeapi.co/api/v2/pokemon/42')
   })
   .then(function () {
   });
+
+//get 30 pokemons by Promise.all
+let p1 = new Promise((resolve, reject) =>{
+  for (var i = 1; i < 11; i++) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      .then(function (response) {
+        console.log(`1-Name: ${response.data.name}`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+      });
+  }
+});
+
+
+let p2 = new Promise((resolve, reject) =>{
+  for (var i = 11; i < 21; i++) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      .then(function (response) {
+        console.log(`2-Name: ${response.data.name}`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+      });
+  }
+});
+
+let p3 = new Promise((resolve, reject) =>{
+  for (var i = 21; i < 31; i++) {
+    axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`)
+      .then(function (response) {
+        console.log(`3-Name: ${response.data.name}`);
+      })
+      .catch(function (error) {
+        console.log(error);
+      })
+      .then(function () {
+      });
+  }
+});
+
+Promise.all([p1, p2, p3]).then(function() {});
