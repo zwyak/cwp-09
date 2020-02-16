fe5011da595152e3bbcb64093ae4aa71dd984213
vlad-senyuk-1997 <vladsenyuk1975@gmail.com>
2020-02-16 11:26:51 +0300

    Added promise.props (error)

--- pokeapi.js
+++ pokeapi.js
@@ -1,6 +1,13 @@
 const Promise = require('bluebird');
 const axios = require('axios');
 
+const getPromise = (url, i) => {
+    return new Promise((resolve, reject) => {
+      const pokemon = axios.get(`${url}${i}`);
+      resolve(pokemon);
+    });
+}
+
 //get pokemon by id=42
 axios.get('https://pokeapi.co/api/v2/pokemon/42')
   .then((response) => {
@@ -11,19 +18,12 @@ axios.get('https://pokeapi.co/api/v2/pokemon/42')
   });
 
 //use Promise.all
-const getPromise = (i) => {
-    return new Promise((resolve, reject) => {
-      const pokemon = axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`);
-      resolve(pokemon);
-    });
-}
-
 let promises = [];
 
 for (let i = 1; i < 4; i++) {
   let pokemons = [];
   for (let j = 1; j < 11; j++) {
-    pokemons.push(getPromise(i * j));
+    pokemons.push(getPromise('https://pokeapi.co/api/v2/pokemon/', i * j));
   }
   promises.push(pokemons);
 }
@@ -42,7 +42,35 @@ Promise.all(promises.map(Promise.all.bind(Promise)))
 
 //use Promise.any
 
-Promise.any([getPromise(1), getPromise(4), getPromise(7)])
+Promise.any([getPromise('https://pokeapi.co/api/v2/pokemon/', 1), getPromise('https://pokeapi.co/api/v2/pokemon/', 4), getPromise('https://pokeapi.co/api/v2/pokemon/', 7)])
       .then((result) =>{
         console.log(`ANY ${result.data.name}`);
+      })
+      .catch(e => {
+        console.log('error: ', e);
+      });
+
+//use Promise.props
+let poks = [];
+let items = [];
+let locations = [];
+
+for (var i = 1; i < 11; i++){
+  poks.push(getPromise('https://pokeapi.co/api/v2/pokemon/', i));
+}
+
+for (var i = 1; i < 11; i++){
+  items.push(getPromise('https://pokeapi.co/api/v2/item/', i));
+}
+
+for (var i = 1; i < 11; i++){
+  locations.push(getPromise('https://pokeapi.co/api/v2/location/', i));
+}
+
+Promise.props({pokemons: poks[0], items: items[0], locations: locations[0]})
+      .then((result) =>{
+        console.log(result.pokemons, result.items, result.locations);
+      })
+      .catch(e => {
+        console.log('error: ', e);
       });
